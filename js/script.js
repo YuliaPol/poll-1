e4892926904eb7fa7b52d6ef4eabf6a5afa8786e
YuliaPol <julia.polyeshko@gmail.com>
2020-05-26 18:13:38 +0300

    valid phone

--- js/script.js
+++ js/script.js
@@ -1,73 +1,69 @@
 jQuery(function ($) {
     $(document).ready(function () {
-        
+
         //mask 
         $('.date').mask('99.99.9999');
         $('.phone').ForceNumericOnly();
-        $('.textarea-group textarea').focusin( function(){
+        $('.textarea-group textarea').focusin(function () {
             $(this).parents('.textarea-group').addClass('focus-mode');
         });
-        $('.textarea-group textarea').focusout( function(){
+        $('.textarea-group textarea').focusout(function () {
             $(this).parents('.textarea-group').removeClass('focus-mode');
         });
-        $(".autoresizing").keyup(function(e) {
-            while($(this).outerHeight() < this.scrollHeight + parseFloat($(this).css("borderTopWidth")) + parseFloat($(this).css("borderBottomWidth"))) {
-                $(this).height($(this).height()+1);
+        $(".autoresizing").keyup(function (e) {
+            while ($(this).outerHeight() < this.scrollHeight + parseFloat($(this).css("borderTopWidth")) + parseFloat($(this).css("borderBottomWidth"))) {
+                $(this).height($(this).height() + 1);
             };
         });
 
-        $('.questions-group textarea').focusin( function(){
+        $('.questions-group textarea').focusin(function () {
             $(this).parents('.textarea-cont').addClass('focus-mode');
         });
-        $('.questions-group textarea').focusout( function(){
+        $('.questions-group textarea').focusout(function () {
             $(this).parents('.textarea-cont').removeClass('focus-mode');
         });
-        $('.audio-upload').click( function(){
+        $('.audio-upload').click(function () {
             $('.file-audio').click();
         });
-        $('.video-upload').click( function(){
+        $('.video-upload').click(function () {
             $('.file-video').click();
         });
-        $('.image-upload').click( function(){
+        $('.image-upload').click(function () {
             $('.file-image').click();
         });
-        $('.file-image').change( function(){
+        $('.file-image').change(function () {
             console.log(this.files[0].name);
             $('.image-name').html(this.files[0].name);
         });
-        $('.file-video').change( function(){
+        $('.file-video').change(function () {
             $('.video-name').html(this.files[0].name);
         });
-        $('.file-audio').change( function(){
+        $('.file-audio').change(function () {
             $('.audio-name').html(this.files[0].name);
             $('.audio-cont img').attr('src', './img/audio-active.png');
         });
     });
 });
 jQuery.fn.ForceNumericOnly =
-function()
-{
-    return this.each(function()
-    {
-        $(this).keydown(function(e)
-        {
-            var max = 15;
-            if ($('.phone').val().length > max) {
-                $('.phone').val($('.phone').val().substr(0, max));
-            }
-            var key = e.charCode || e.keyCode || 0;
-            // allow backspace, tab, delete, enter, arrows, numbers and keypad numbers ONLY
-            // home, end, period, and numpad decimal
-            return (
-                key == 8 || 
-                key == 9 ||
-                key == 13 ||
-                key == 46 ||
-                key == 110 ||
-                key == 190 ||
-                (key >= 35 && key <= 40) ||
-                (key >= 48 && key <= 57) ||
-                (key >= 96 && key <= 105));
+    function () {
+        return this.each(function () {
+            $(this).keydown(function (e) {
+                var max = 15;
+                if ($('.phone').val().length < max) {
+                    var key = e.charCode || e.keyCode || 0;
+                    // allow backspace, tab, delete, enter, arrows, numbers and keypad numbers ONLY
+                    // home, end, period, and numpad decimal
+                    return (
+                        key == 8 ||
+                        key == 9 ||
+                        key == 13 ||
+                        key == 46 ||
+                        key == 110 ||
+                        key == 190 ||
+                        (key >= 35 && key <= 40) ||
+                        (key >= 48 && key <= 57) ||
+                        (key >= 96 && key <= 105));
+                }
+            });
         });
-    });
-};
+    };
